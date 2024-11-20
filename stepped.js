d0e62cd8240247f32f4b182914f4c27f0cb60760
abushahed670 <abu.shahed670@gmail.com>
2024-11-20 22:44:38 +0600

    multiple conditionals added

--- stepped.js
+++ stepped.js
@@ -11,7 +11,7 @@ if(price >= 5000){
 }
 else if(price >2500){
     //5% discount 
-    const discount = price * 10/100;
+    const discount = price * 5/100;
     const payAmount = price - discount;
     console.log(payAmount)
 }
